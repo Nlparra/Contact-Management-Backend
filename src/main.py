54c42a276b03b6e5c7b7235b0dfe2fd7a639a3a3
Nelson <nlparra.dev@gmail.com>
2020-05-01 00:31:51 +0000

    work on back end

--- src/main.py
+++ src/main.py
@@ -7,7 +7,7 @@ from flask_migrate import Migrate
 from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
-from models import db
+from models import db,Contact
 #from models import Person
 
 app = Flask(__name__)
@@ -28,14 +28,92 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
+@app.route('/contact', methods=['POST', 'GET'])
+def handle_contact():
 
-    response_body = {
-        "hello": "world"
-    }
+    if request.method == "POST":
+        body = request.get_json()
+        
+        if body is None:
+            raise APIException("You need to specify the request body as a json object",status_code = 400)
+        if "name" not in body:
+            raise APIException("You need to specify the name", status_code = 400)
+        if "email" not in body:
+            raise APIException("You need to specify the email", status_code = 400)
+        if "phone" not in body:
+            raise APIException("You need to specify the phone", status_code = 400)
+        if "address" not in body:
+            body["address"] = None
+        
+        contact1 = Contact(name=body["name"],email=body["email"],phone=body["phone"],address=body["address"])
+        db.session.add(contact1)
+        db.session.commit()
 
-    return jsonify(response_body), 200
+        return "Ok", 200
+    
+    #Get Request
+    if request.method == "GET":
+        
+        all_contact = Contact.query.all()
+        all_contact = list(map(lambda x : x.serialize(), all_contact))
+        return jsonify(all_contact), 200
+
+    return "Invalid Method", 404
+
+ 
+@app.route('/contact/<int:contact_id>', methods=['PUT', 'GET','DELETE'])
+def single_contact(contact_id):
+
+    #PUT Request
+    if request.method == "PUT":
+
+        body = request.get_json()
+        
+        if body is None:
+            raise APIException("You need to specify the request body as a json object",status_code = 400)
+        contact1 = Contact.query.get(contact_id)
+        if contact1 is None:
+            raise APIException("User not found!",404)
+        
+        if "name" in body:
+            contact1.name = body["name"]
+        if "email" in body:
+            contact1.email = body["email"]
+        if "phone" in body:
+            contact1.phone = body["phone"]
+        if "address" in body:
+            contact1.address = body["address"]
+        
+        db.session.commit()
+
+        return jsonify(contact1.serialize()),200 
+
+    #GET method
+    if request.method == "GET":
+        contact1 = Contact.query.get(contact_id)
+        if contact1 is None:
+            raise APIException("User not found!", status_code = 404)
+
+        return jsonify(contact1.serialize()),200
+        
+    #DELETE method
+    if request.method == "DELETE":
+        contact1 = Contact.query.get(contact_id)
+        if contact1 is None:
+            raise APIException("User not found!", status_code = 404)
+
+        db.session.delete(contact1)
+        db.session.commit()
+
+        return "Ok",200
+        
+    return "Invalid method", 404
+
+
+
+
+
+
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -2,16 +2,21 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-# class Person(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     username = db.Column(db.String(80), unique=True, nullable=False)
-#     email = db.Column(db.String(120), unique=True, nullable=False)
+class Contact(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=False, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    phone = db.Column(db.String(120), unique=True, nullable=False)
+    address = db.Column(db.String(120), unique=False, nullable=True)
 
-#     def __repr__(self):
-#         return '<Person %r>' % self.username
+    def __repr__(self):
+        return '<Contact %r>' % self.name
 
-#     def serialize(self):
-#         return {
-#             "username": self.username,
-#             "email": self.email
-#         }
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "email": self.email,
+            'phone': self.phone,
+            "address": self.address
+        }
